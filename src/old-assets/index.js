b8571d616fdf37c1c754185aeafb183e8e0996b2
azizarwn <azzahrawani98@gmail.com>
2021-06-22 15:36:18 +0700

    react

---
+++ src/old-assets/index.js
@@ -0,0 +1,13 @@
+
+// var text = document.getElementsByClassName("blinkColor");
+// setInterval(function () {
+//     text[0].style.color = "white";
+//     // text[1].style.color = "blue";
+//     // text[2].style.color = "green";
+
+//     setTimeout(function () {
+//         text[0].style.color = "#c2d9ff";
+//         // text[1].style.color = "black";
+//         // text[2].style.color = "black"; 
+//     }, 500);
+// }, 1200);
